fa8e25b9ef036e747e36b2048412b8d398a09e84
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-03-31 20:24:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const quotes = ["I am the chosen one.", "Keep playing.", "Learn the game.", "Know the game.", "Don't let your guard down."];
+
+function generateRandomQuotes() {
+
+    const quote = Math.floor(Math.random() *quotes.length);
+    return quotes[quote];
+};
+
+function displayQuote(){
+let text = document.getElementById('quote');
+text.innerHTML= generateRandomQuotes();
+};
+
+console.log(generateRandomQuotes());
+console.log(quotes[2]);
